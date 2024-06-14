4ca5cb6c4bb6a253438327d07d2b3055815280d1
diegovndres <diega.silvar@duocuc.cl>
2024-06-14 14:09:34 -0400

    se crea archivo de funciones

--- proyecto.py
+++ proyecto.py
@@ -1,7 +1,7 @@
 import os, time
+from funciones import * 
 
 
-trabajadores = []
 cargos = ("CEO","DESARROLLADOR","ANALISTA")
 while True:
     print("MENU TRABAJADORES")
@@ -12,16 +12,7 @@ while True:
     opc =  int(input("Ingrese opcion: "))
     os.system('cls')
     if opc == 1:
-        print("REGISTRO TRABAJADOR")
-        nombre_apellido = input("Ingrese nombre y apellido: ")
-        cargo = int(input("Ingrese cargo(1. CEO 2. DESARROLLADOR 3. ANALISTA): "))
-        sueldo_bruto = int(input("Ingrese sueldo bruto: "))
-        desc_salud = 7/100 * sueldo_bruto
-        desc_afp = int(12/100 * sueldo_bruto)
-        sueldo_liquido = sueldo_bruto * 0.81
-        trabajador = [nombre_apellido,cargo[cargo-1],sueldo_bruto,desc_salud,desc_afp,sueldo_liquido]
-        trabajadores.append(trabajador)
-        print("TRABAJADOR REGISTRADO")
+        registrar_trabajador()
 
     elif opc == 2:
         pass
